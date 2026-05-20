721815a784f39762a7ccb92fb01e39dc44a99568
AK <akashkumar200405@gmail.com>
2026-05-20 15:16:21 +0530

    Cost Function Added along with Cost vs epochs graph

--- linearregression.py
+++ linearregression.py
@@ -6,6 +6,17 @@ data=pd.read_csv('data.csv')
 # plt.scatter(data.Hours_Studied,data.Exam_Score)
 # plt.show()
 
+def Mean_Squarred_error(m,b,points):
+    E=0
+    n=len(points)
+    for i in range(n):
+        x=points.iloc[i].Hours_Studied
+        y=points.iloc[i].Exam_Score
+
+        E+=(y-(m*x+b))**2
+
+    return E/n
+
 def gradient_descent(m_now,b_now,L,points):
     m_grad=0
     b_grad=0
@@ -28,18 +39,34 @@ def gradient_descent(m_now,b_now,L,points):
 
     return m,b
 
+#Pass Values
+
 m=0
 b=0
-L=0.0001
-epochs=300   #No. of iterations
+L=0.00001
+epochs=300 #No. of iterations
 
+epoch_plot=[]  
+costs=[]
 for i in range(epochs):
     m,b=gradient_descent(m,b,L,data)
+    cost=Mean_Squarred_error(m,b,data)
+    costs.append(cost)
+    if(i%50==0):   
+        # epoch_plot.append(i)
+        print(
+            f"Epoch:{i}, "
+            f"m:{m:.3f}, "
+            f"b:{b:.3f}, "
+            f"Cost:{cost:.3f}"
+        )
 
-print (m,b)
+
 
 plt.scatter(data.Hours_Studied,data.Exam_Score)
-plt.plot(list(range(1,16)), [m*x+b for x in range(1,16)], color='red')
+# x=data.Hours_Studied
+# plt.plot(x, [m*i+b for i in x], color='red')
+# plt.plot(range(epochs),costs, color='green')
 plt.show()
 
 
